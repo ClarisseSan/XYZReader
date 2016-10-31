25f2e4395f792f15e0bc2d6e53a1c6e10f4b3426
isse <sanchezclarisse@gmail.com>
2016-10-31 21:50:20 +0800

    feat: apply aspect ratio image

---
+++ XYZReader/src/main/java/com/example/xyzreader/view/CustomImageView.java
@@ -0,0 +1,38 @@
+package com.example.xyzreader.view;
+
+import android.content.Context;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+/**
+ * Created by isse on 31/10/2016.
+ */
+
+public class CustomImageView extends ImageView {
+    public CustomImageView(Context context) {
+        super(context);
+    }
+
+    public CustomImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public CustomImageView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int ratio = MeasureSpec.getSize(widthMeasureSpec) * 3/7;
+        int ratioHeightSpec = MeasureSpec.makeMeasureSpec(ratio,MeasureSpec.EXACTLY);
+        super.onMeasure(widthMeasureSpec, ratioHeightSpec);
+
+    }
+}
