0e9e3eabd88d70792e1ae7cac0c247d50ed339fc
isse <sanchezclarisse@gmail.com>
2016-11-04 13:15:12 +0800

    feat: add license

--- XYZReader/src/main/java/com/example/xyzreader/remote/Config.java
+++ XYZReader/src/main/java/com/example/xyzreader/remote/Config.java
@@ -1,8 +1,18 @@
+/*
+* Copyright 2016 Angela Sanchez
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+* */
+
 package com.example.xyzreader.remote;
 
-import org.json.JSONException;
-
-import java.net.MalformedURLException;
 import java.net.URL;
 
 public class Config {
@@ -11,9 +21,9 @@ public class Config {
     static {
         URL url = null;
         try {
-            url = new URL("https://dl.dropboxusercontent.com/u/231329/xyzreader_data/data.json" );
+            url = new URL("https://dl.dropboxusercontent.com/u/231329/xyzreader_data/data.json");
         } catch (Exception e) {
-             e.getMessage();
+            e.getMessage();
         }
 
         BASE_URL = url;
